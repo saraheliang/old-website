60778dfad8daa09f39951e5a4fc57c4bb4a80305
Sarah Liang <seliang@usc.edu>
2022-06-07 09:37:59 -0700

    MUI

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,32 @@
 // when creating a component, always import react from React
 import React from 'react';
 import './Home.css';
-import Button from '@mui/material/Button';
-import { Link } from "react-router-dom";
 import NavBar from "./NavBar";
+
+import Button from '@mui/material/Button';
+import Popover from '@mui/material/Popover';
+import Typography from '@mui/material/Typography';
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 // create a component by writing a function
 function Home() {
-  return(
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+    setAnchorEl(null);
+    };
+    
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+
+    return(
     <>
         <NavBar />
         <div className="home-content">
@@ -23,12 +42,30 @@ function Home() {
                 <Link id="see-my-work-link" to="/portfolio"><Button id="MUI-see-my-work" variant="contained">see my work</Button></Link>
             </div>
             <div id="contact-logos">
-                circle
+                <a href="https://github.com/saraheliang" target="_blank"><FontAwesomeIcon class="icon no-space" icon={faGithub} /></a>
+                <a href="https://www.linkedin.com/in/saraheliang/" target="_blank"><FontAwesomeIcon class="icon" icon={faLinkedin} /></a>
+                {/* on click MUI popover */}
+                <a aria-describedby={id} onClick={handleClick}><FontAwesomeIcon class="icon" icon={faEnvelope}  /></a>
+                <Popover
+                    id={id}
+                    open={open}
+                    anchorEl={anchorEl}
+                    onClose={handleClose}
+                    anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                    }}
+                >
+                    <Typography class="center-this"><span id="connect-msg">^_^ Let's Connect!</span><br/> <span id="email-msg">seliang@usc.edu</span> 
+                    </Typography>
+                </Popover>
             </div>
+            {/* scroll to top button (FAB- floating action button) */}
+            <p class="scroll-to-top">scroll to top here, interactivity + FAB</p>
         </div>
-        <div className="forMore">
-            <p>hi</p>
-        </div>
+        {/* <div className="forMore">
+
+        </div> */}
     </>
   );
 }
